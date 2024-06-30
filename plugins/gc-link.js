78c69716737e3189da15e79d19acc6ae692e0ef4
Imadkinge <imadchat47@gmail.com>
2024-06-30 21:08:23 +0100

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,15 +3,15 @@ import fs from 'fs';
 const handler = async (m, {conn, args}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
   const tradutor = _translate.plugins.gc_link
 
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
       title: tradutor.texto1[0],
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
-      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
+      body: 'bobiza♥️ - 𝙱𝚘𝚝',
+      previewType: 0, thumbnail: fs.readFileSync('./'),
       sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}});
 };
 handler.help = ['linkgroup'];
